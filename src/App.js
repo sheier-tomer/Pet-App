04eab1abf9b582fd8bea5d7fbb8a7c7ce193cf56
Steven Tan <steventan1380@gmail.com>
2023-04-19 02:46:57 -0400

    fixing user pool error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React, { useState } from 'react';
 import Form from './components/input.jsx';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'
+import { Auth } from "aws-amplify";
+
+// Configure Amplify in index file or root file
+Amplify.configure({
+  Auth: {
+    region: awsExports.REGION,
+    userPoolId: awsExports.USER_POOL_ID,
+    userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID
+  }
+})
 
 function App() {
   const [data, setData] = useState(null);
@@ -17,6 +31,16 @@ function App() {
       .catch(error => {
         console.error('Error:', error);
       });
+      return (
+        <Authenticator>
+          {({ signOut, user }) => (
+              <div>
+                <p>Welcome {user.username}</p>
+                <button onClick={signOut}>Sign out</button>
+              </div>
+          )}
+        </Authenticator>
+    );
   }
 
   return (
