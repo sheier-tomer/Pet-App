dd47b1e3d9bed7f702e1fdedad68b3032cf49a43
philipyeh12 <philipyeh12@gmail.com>
2023-04-24 02:12:38 -0400

    style,back button and rendering

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from './components/input.jsx';
 import axios from 'axios';
+import './App.css';
 
 const API_ENDPOINT = "https://crx7wfl54d.execute-api.us-east-1.amazonaws.com/default/get-presigned-url";
-
 
 function App() {
   const [data, setData] = useState(null);
   const [showData, setShowData] = useState(false);
   const [images, setImages] = useState([]);
 
+  useEffect(() => {
+    fetchImagesFromS3();
+  }, []);
+
   const handleClick = () => {
     fetch('https://wgohphvw18.execute-api.us-east-1.amazonaws.com/default/petFetch')
       .then(response => response.json())
       .then(data => {
         const parsedData = JSON.parse(data.body);
-        setData(parsedData);
+        setData(parsedData.Items);
         setShowData(true);
         console.log(data);
-
-        fetchImagesFromS3();
       })
       .catch(error => {
         console.error('Error:', error);
       });
-
-    
   }
-
 
   const fetchImagesFromS3 = async () => {
     try {
@@ -37,7 +36,7 @@ function App() {
         params: { fetchImages: true }
       });
       
-      setImages(imagesResponse.data);
+      setImages(imagesResponse.data.images);
     } catch (error) {
       console.error(error);
       console.log("Fetching images from S3 failed");
@@ -45,25 +44,37 @@ function App() {
     }
   };
 
+  const itemsWithImages = data && images ? data.map((item, index) => {
+    return {
+      data: item,
+      imageUrl: images[index]
+    };
+  }) : [];
+
+  const handleBackClick = () => {
+    window.location.reload();
+  };
 
   return (
+    
     <div>
+      <h2 className="page-title">Welcome to The Pet Gazette</h2>
       {!showData && <Form />}
-      <button onClick={handleClick}>See photos</button>
-      {showData && data && data.Items && (
-  <ul>
-    {data.Items.map((item, index) => (
-      <li key={item.uid}>
-        {item.pet} - {item.type} - {item.message} - {item.fileName} 
-        {images.images && images.images[index] && (
-          <img src={images.images[index]} alt={`Pet Image ${index}`} />
-        )}
-      </li>
-    ))}
-  </ul>
-)}
-
-      
+      <button className="see-photos-button" onClick={handleClick}>See photos</button>
+      <button className="see-photos-button" onClick={handleBackClick}>Back</button>
+      {showData && (
+        <div>
+          <ul>
+            {itemsWithImages.map(({ data, imageUrl }) => (
+              <li key={data.uid}>
+                {data.pet} - {data.type} - {data.message}
+                {imageUrl && <img src={imageUrl} alt={`Pet Image`} />}
+                
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
